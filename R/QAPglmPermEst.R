c151f82569fdeca55c51947074dd5c0d8f2ad38e
Robert Krause <43546240+RWKrause@users.noreply.github.com>
2025-02-09 09:57:55 -0500

    Update QAPglmPermEst.R

--- R/QAPglmPermEst.R
+++ R/QAPglmPermEst.R
@@ -80,6 +80,8 @@ QAPglmPermEst <- function(i,
 
     pred <- Reduce(f = 'rbind', pred_list)
   }
+  
+  xv. <- as.matrix(pred[,4:nx])
 
   if (!rand.) {
     pm  <- glm(mod., data = pred, family = family.)
